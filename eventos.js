a8216965ebff449a8f6af1a61ed957f8a859c5a8
Shinx606 <224807183+mescuatro@users.noreply.github.com>
2025-08-12 22:08:54 -0300

    La solución

--- eventos.js
+++ eventos.js
@@ -3,3 +3,8 @@ document.getElementById("boton").addEventListener("click", function () {
      alert("¡Hola! Soy el div");
   });
 });
+
+function hola(event){
+    event.stopPropagation();
+    alert("¡Hola!")
+}
